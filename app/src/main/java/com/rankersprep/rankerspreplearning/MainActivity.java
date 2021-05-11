c09e72dccf50e31f275b83ad104b1459ecb2438f
harshitmawandia <hmawandia@gmail.com>
2021-05-12 05:02:35 +0530

    bottom navigation set up

--- app/src/main/java/com/rankersprep/rankerspreplearning/MainActivity.java
+++ app/src/main/java/com/rankersprep/rankerspreplearning/MainActivity.java
@@ -18,6 +18,11 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void signIn(View view){
+        Intent intent = new Intent(this,AdminActivity.class);
+        startActivity(intent);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
